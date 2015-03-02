62feba555eb9ff56b8d962301e81f1cc762b9ee3
John <jford@redventures.com>
2015-03-01 20:36:39 -0500

    Updates to routes, composer, and readme file

--- README.md
+++ README.md
@@ -14,8 +14,12 @@ After composer is installed, use it fetch the silex framework (and any other dep
 
 	$ composer.phar install
 
-For other installation options, see the See the [composer documentation](https://getcomposer.org/doc/00-intro.md) for assistance.
+For other installation options, see the [composer documentation](https://getcomposer.org/doc/00-intro.md) for assistance.
 
 ## Silex
 
 On Apache, you may need to make changes to your .htaccess file, particularly if the app isn't at the root directory. For other web servers and assistance, see the [silex documentation](http://silex.sensiolabs.org/doc/web_servers.html).
+
+## Database
+
+The schema used by the app can be found in the schema folder with a file for each of the various tables used. In order to setup your connection, populate the database.json file located in the config folder with the values for your connection. Initial data sets to load the City, State, and User table can be found in the test folder.
--- index.php
+++ index.php
@@ -1,66 +1,62 @@
 <?php
-namespace DemoGeoAPI;
 require_once 'vendor/autoload.php';
 
-//-- Setup autoloader
-spl_autoload_register(
-    $localAutoload = function ($className)
-    {
-        if (stripos($className, 'DemoGeoAPI\\') === 0) {
-            $className = str_replace('DemoGeoAPI\\', '', $className);
-            $className = str_replace('\\', '/', $className) . '.php';
+use DemoGeoAPI\Application;
+use DemoGeoAPI\Controllers;
+use Symfony\Component\HttpFoundation\Request;
 
-            if (is_file($className)) {
-                require_once($className);
-                return true;
-            }
-        }
-        
-        return false;
-    },
-    false,
-    true
-);
+// Initialize the application
+$App = new Application();
 
-use \Symfony\Component\HttpFoundation\Request;
+// Perform basic setup
+$App->setup();
 
-$App = new \Silex\Application();
-
+// Uncomment to enable debugging
 $App['debug'] = true;
 
-$App->get('/hello/{name}', function ($name) use ($App) {
-	return 'Hello '.$App->escape($name);
-});
+/** BEGIN ROUTES **/
 
+// Retrieve all cities in a given state
 $App->get('/v1/states/{state}/cities', function($state, Request $Request) use ($App) {
     $Controller = new Controllers\CityController($App, $Request);
-    return $Controller->fetchCitiesByState($state);
+    return $Controller->getCitiesByState($state);
 });
 
+// Retrieve all cities in a given radius
 $App->get('/v1/states/{state}/cities/{city}', function($state, $city, Request $Request) use ($App) {
     $radius = $Request->query->get('radius', false);
 
     if(empty($radius)){
-        $App->abort(404, "No radius passed to find cities in range.");
+        $App->abort(400, "No radius passed to find cities in range.");
     }
 
     $Controller = new Controllers\CityController($App, $Request);
-    return $Controller->fetchCitiesByRadius($state, $city, $radius);
+    return $Controller->getCitiesByRadius($city, $state, $radius);
 });
 
-$App->match('/v1/users/{user}/visits', function($user, Request $Request) use ($App) {
+// Retrieve a given user
+$App->get('/v1/users/{userId}', function($userId, Request $Request) use ($App) {
+    $Controller = new Controllers\UserController($App, $Request);
+    return $Controller->getUser($userId);
+});
+
+// Perform actions on user visits
+$App->match('/v1/users/{userId}/visits', function($userId, Request $Request) use ($App) {
     $Controller = new Controllers\VisitController($App, $Request);
 
     switch ($Request->getMethod()) {
+
         case "POST":
-            return $Controller->createVisits($user);
+            return $Controller->createVisits($userId);
 
         case "PUT":
-            return $Controller->updateVisits($user);
+            return $Controller->updateVisits($userId);
 
         case "GET":
-            return $Controller->getVisits($user);
+            return $Controller->getVisits($userId);
     }
 });
+
+/** END ROUTES **/
 
 $App->run();
